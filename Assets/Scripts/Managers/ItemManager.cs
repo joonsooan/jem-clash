148a30f1460cb3e729a0dab05a330e0ddfc2fab5
joonsooan <joonsooan@gmail.com>
2025-02-09 15:07:00 +0900

    Fixed double click bug

--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -13,15 +13,12 @@ public class ItemManager : MonoBehaviour
 
         GameObject buttonObj = Instantiate(itemBtnPrefab, transform);
         Upgrade upgrade = buttonObj.GetComponent<Upgrade>();
-        
+
         if (upgrade != null)
         {
             upgrade.upgradeData = data;
             buttonObj.GetComponent<Image>().sprite = upgrade.upgradeData.itemImage;
             upgrade.level = 0;
-
-            Button btn = buttonObj.GetComponent<Button>();
-            btn.onClick.AddListener(upgrade.OnClick);
         }
     }
 }
