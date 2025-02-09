64d22a77e967be1237f4d3202ffb537ec4850df0
joonsooan <joonsooan@gmail.com>
2025-02-09 09:54:41 +0900

    Created ItemManager script
    아이템을 관리하는 스크립트 생성 + 임시 저장

---
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public List<UpgradeData> items = new();
+
+    public void CreateItemBtn()
+    {
+        
+    }
+}
